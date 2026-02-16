a13d97b0273e3411ce7c2b7219686311e3c39d7e
chineseYouth <revolution202781@outlook.com>
2026-02-16 14:19:29 +0800

    完善Resource

--- engine/ecs/resource/resource.h
+++ engine/ecs/resource/resource.h
@@ -3,18 +3,19 @@
 #include <typeindex>
 
 #include "engine/macros.h"
+#include "engine/memory/allocator.h"
 
 namespace fe::engine::ecs {
 struct Resource {
   Resource() = default;
-  ~Resource() { Destroy(); }
+  ~Resource() { destroy(); }
 
   Resource(const Resource&) = delete;
   Resource& operator=(const Resource&) = delete;
 
-  Resource(Resource&& other) noexcept : m_Ptr(other.m_Ptr), m_Deleter(other.m_Deleter) {
-    other.m_Ptr = nullptr;
-    other.m_Deleter = nullptr;
+  Resource(Resource&& other) noexcept : m_ptr(other.m_ptr), m_deleter(other.m_deleter) {
+    other.m_ptr = nullptr;
+    other.m_deleter = nullptr;
 
 #if defined(FE_DEBUG)
     m_TypeInfo = other.m_TypeInfo;
@@ -24,11 +25,11 @@ struct Resource {
 
   Resource& operator=(Resource&& other) noexcept {
     if (this != &other) {
-      Destroy();
-      m_Ptr = other.m_Ptr;
-      m_Deleter = other.m_Deleter;
-      other.m_Ptr = nullptr;
-      other.m_Deleter = nullptr;
+      destroy();
+      m_ptr = other.m_ptr;
+      m_deleter = other.m_deleter;
+      other.m_ptr = nullptr;
+      other.m_deleter = nullptr;
 
 #if defined(FE_DEBUG)
       m_TypeInfo = other.m_TypeInfo;
@@ -38,64 +39,52 @@ struct Resource {
     return *this;
   }
 
-  template <typename T>
-  void SetPtr(T* ptr, bool needFree = true) {
-    Destroy();
-
-    if (ptr != nullptr) {
-#if defined(FE_DEBUG)
-      m_TypeInfo = typeid(T);
-#endif
-
-      if (needFree) {
-        m_Deleter = [](void* p) {
-          if (p) {
-            GAlloc::destroy<T>(static_cast<T*>(p));
-          }
-        };
-      }
-
-      m_Ptr = ptr;
-    }
-  }
-
   template <typename T, typename... Args>
-  T* Create(Args&&... args) {
-    T* ptr = GAlloc::create<T>(std::forward<Args>(args)...);
-    SetPtr<T>(ptr, true);
-    return ptr;
+  static Resource create(Args&&... args) {
+    Resource res;
+    res.m_ptr = memory::Allocator::create<T>(std::forward<Args>(args)...);
+    res.m_deleter = [](void* p) {
+      if (p) {
+        memory::Allocator::destroy<T>(static_cast<T*>(p));
+      }
+    };
+#if defined(FE_DEBUG)
+    res.m_TypeInfo = typeid(T);  // 必须添加这一行
+#endif
+    return res;
   }
 
-  bool Valid() const { return m_Ptr != nullptr; }
+  bool valid() const { return m_ptr != nullptr; }
 
   template <typename T>
-  const T* Get() const {
-    FE_ASSERT(Valid() && m_TypeInfo == typeid(T), "Type mismatch!");
-    return static_cast<const T*>(m_Ptr);
+  const T* get() const {
+    FE_ASSERT(valid() && m_TypeInfo == typeid(T), "Type mismatch!");
+    return static_cast<const T*>(m_ptr);
   }
 
   template <typename T>
-  T* Get() {
-    return const_cast<T*>(static_cast<const Resource*>(this)->Get<T>());
+  T* get() {
+    FE_ASSERT(valid() && m_TypeInfo == typeid(T), "Type mismatch!");
+    return static_cast<T*>(m_ptr);
   }
 
-  FE_FINLINE const void* GetPtr() const { return m_Ptr; }
-  FE_FINLINE void* GetPtr() { return const_cast<void*>(static_cast<const Resource*>(this)->GetPtr()); }
+  FE_FINLINE const void* getPtr() const { return m_ptr; }
+  FE_FINLINE void* getPtr() { return m_ptr; }
 
-  FE_FINLINE void Destroy() {
-    if (m_Ptr && m_Deleter) {
-      m_Deleter(m_Ptr);
+  FE_FINLINE void destroy() {
+    if (m_ptr && m_deleter) {
+      m_deleter(m_ptr);
     }
-    m_Ptr = nullptr;
-    m_Deleter = nullptr;
+    m_ptr = nullptr;
+    m_deleter = nullptr;
 #if defined(FE_DEBUG)
     m_TypeInfo = typeid(void);
 #endif
   }
 
  private:
-  void* m_Ptr{nullptr};
-  void (*m_Deleter)(void*) = nullptr;
+  void* m_ptr{nullptr};
+  void (*m_deleter)(void*) = nullptr;
 #if defined(FE_DEBUG)
   std::type_index m_TypeInfo = typeid(void);
 #endif
